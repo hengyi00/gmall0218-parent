772a21c7079430f36c68abe75bf305040bb5c027
zhuefeng <zhufeng9664@qq.com>
2019-07-23 20:36:44 +0800

    Publisher

--- gmall0218-publisher/src/main/java/com/atguigu/gmall0218/publisher/service/impl/PublisherServiceImpl.java
+++ gmall0218-publisher/src/main/java/com/atguigu/gmall0218/publisher/service/impl/PublisherServiceImpl.java
@@ -1,6 +1,7 @@
 package com.atguigu.gmall0218.publisher.service.impl;
 
 import com.atguigu.gmall0218.publisher.mapper.DauMapper;
+import com.atguigu.gmall0218.publisher.mapper.OrderMapper;
 import com.atguigu.gmall0218.publisher.service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,23 +16,41 @@ public class PublisherServiceImpl implements PublisherService {
     @Autowired
     DauMapper dauMapper;
 
+    @Autowired
+    OrderMapper orderMapper;
+
     @Override
     public int getDauTotal(String date) {
-        int dauTotal = dauMapper.getDauTotal(date);
-        return dauTotal;
+        return dauMapper.getDauTotal(date);
     }
 
     @Override
     public Map getDauHour(String date) {
+        HashMap dauHourMap=new HashMap();
         List<Map> dauHourList = dauMapper.getDauHour(date);
-        //把 List<Map> 转换成  Map
-        Map duaHourMap=new HashMap();
         for (Map map : dauHourList) {
-            String loghour =(String ) map.get("LOGHOUR");
-            Long ct =(Long ) map.get("CT");
-            duaHourMap.put(loghour,ct);
-
+            dauHourMap.put(map.get("LOGHOUR"),map.get("CT"));
         }
-        return duaHourMap;
+
+        return dauHourMap;
     }
+
+    @Override
+    public Double getOrderAmountTotal(String date) {
+        return orderMapper.getOrderAmountTotal(date);
+    }
+
+    @Override
+    public Map getOrderAmountHour(String date) {
+        //调整一下map结构
+        List<Map> orderAmountHourList = orderMapper.getOrderAmountHour(date);
+
+        Map orderAmountMap=new HashMap();
+        for (Map map : orderAmountHourList) {
+            orderAmountMap.put( map.get("CREATE_HOUR"),map.get("ORDER_AMOUNT"));
+        }
+
+        return orderAmountMap;
+    }
+
 }
